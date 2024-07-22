10a06b6b76c1587ae471489a6c88d7aa0ac95196
Abhicodeswell <117502903+Abhicodeswell@users.noreply.github.com>
2024-07-22 15:02:04 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap{
+
+  public static void main(String[] args){
+    int a  =5;
+    int b = 10;
+    // swap
+    int t = a;
+    a = b;
+    b = temp;
+
+    System.out.println("a\t" + a + "\nb\t" + b);
+  }
+ }
